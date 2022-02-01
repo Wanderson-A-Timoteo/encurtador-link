d3165f033a7ab07659fba3c1e157f0c5258e1d89
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-01 15:39:21 -0400

    Adicionado efeito em arrow

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -7,7 +7,7 @@ export default function Links() {
     <div className="links-container">
       <div className="links-header">
         <Link to="/">
-          <FiArrowLeft size={38} color="#FFF" />
+          <FiArrowLeft className="link-arrow-left" size={38} color="#FFF" />
         </Link>
         <h1>Meus Links</h1>
       </div>
